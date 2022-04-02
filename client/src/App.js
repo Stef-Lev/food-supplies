d3bab731822a7524a36a1c8922011b0ced17447e
Stefanos Leventis <sigianas@yahoo.com>
2022-04-03 01:30:48 +0300

    added barcode package

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Example from "./components/Example/Example";
+import Home from "./routes/Home/Home";
 
 function App() {
   return (
     <div className="App">
-      <h1>You are ready to start your MERN stack Project</h1>
-      <Example />
       <Router>
         <Routes>
           {/* Protected route example */}
-          {/* <Route
-            exact
-            path="/"
-            element={
-              <PrivateRoute>
-                <Home />
-              </PrivateRoute>
-            }
-          /> */}
+          <Route exact path="/" element={<Home />} />
           {/* Simple Route example */}
           {/* <Route exact path="/example" element={<LoginPage />} /> */}
         </Routes>
---
+++ client/src/routes/Home/Home.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
+
+function Home() {
+  const [scan, setScan] = useState(false);
+  const [logs, setLog] = useState([]);
+
+  const barcodeScannerComponentHandleUpdate = (error, result) => {
+    if (result) {
+      setLog([...logs, result.text]);
+      window.navigator.vibrate(100);
+      setScan(false);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={() => setScan(true)}>SCAN</button>
+      <button onClick={() => setScan(false)}>CANCEL</button>
+      {scan && (
+        <div>
+          {/* <QrReader
+            delay={300}
+            onScan={qrReaderHandleScan}
+            onError={qrReaderHandleError}
+          /> */}
+          <BarcodeScannerComponent
+            onUpdate={barcodeScannerComponentHandleUpdate}
+          />
+        </div>
+      )}
+      <div>
+        {logs.map((log) => (
+          <div key={log}>{log}</div>
+        ))}
+
+        <button onClick={() => setLog([])}>CLEAR</button>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
