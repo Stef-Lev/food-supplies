e371190381e81f27a9584baa1953268d7e6db47e
Stefanos Leventis <sigianas@yahoo.com>
2022-04-06 00:33:11 +0300

    added sound

--- client/src/routes/AddProductsPage/AddProductsPage.js
+++ client/src/routes/AddProductsPage/AddProductsPage.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import BarcodeScannerComponent from "react-qr-barcode-scanner";
 import Button from "@material-ui/core/Button";
+import soundfile from "../../sounds/blip.mp3";
 
 function AddProductsPage() {
   const [scannerOn, setScannerOn] = useState(false);
   const [code, setCode] = useState([]);
+  const audio = new Audio(soundfile);
 
   const handleUpdate = (error, result) => {
     if (result) {
       setCode(result.text);
+      audio.play();
       window.navigator.vibrate(100);
       setScannerOn(false);
     }
