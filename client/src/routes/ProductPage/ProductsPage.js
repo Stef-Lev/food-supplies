a0d56754dfbbee02a52ed249b6b765e76770e495
Stefanos Leventis <sleventis@ph.esl-asia.com>
2022-06-24 17:52:06 +0300

    bawic mapping of products

--- client/src/routes/ProductPage/ProductsPage.js
+++ client/src/routes/ProductPage/ProductsPage.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { fetchMethod } from "../../utils/fetchMethod";
+import { UserContext } from "../../context/UserContext";
 
 function ProductsPage() {
-  return <div>ProductPage</div>;
+  const [products, setProducts] = useState([]);
+  const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    let mounted = true;
+    if (mounted) {
+      fetchMethod("get", `/api/user/${user._id}`).then((data) =>
+        setProducts(data.user.products)
+      );
+    }
+
+    return () => (mounted = false);
+  }, [user._id]);
+
+  return (
+    <div>
+      <h2>ProductPage</h2>
+
+      {products &&
+        products.length > 0 &&
+        products.map((item, index) => (
+          <p key={`product_${index + 1}`}>{item.title}</p>
+        ))}
+    </div>
+  );
 }
 
 export default ProductsPage;
