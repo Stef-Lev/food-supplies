ed0f5777caf3c3bb5173479ebdefef64577a7296
Stefanos Leventis <sleventis@ph.esl-asia.com>
2022-07-15 23:37:27 +0300

    added styling to product page

--- client/src/routes/ProductPage/ProductPage.js
+++ client/src/routes/ProductPage/ProductPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../../context/UserContext";
+import styles from "./ProductPage.module.css";
 
 function ProductPage() {
   const { user } = useContext(UserContext);
@@ -17,22 +18,26 @@ function ProductPage() {
     return () => (mounted = false);
   }, [productid, user.products]);
 
-  console.log(product);
+  if (product._id) {
+    console.log(
+      "lists",
+      product,
+      product.lists.map((item) => item)
+    );
+  }
 
   return (
-    <div>
-      {/* {product && (
+    <div className={styles.product_info_container}>
+      {product._id && (
         <>
           <h2>{product.title}</h2>
           <ul>
-            {product.lists.map((item) => (
-              <li>
-                {item.listName}: {item.quantity}
-              </li>
+            {product.lists.map((item, index) => (
+              <li key={`product_${index + 1}`}>{item.listName}</li>
             ))}
           </ul>
         </>
-      )} */}
+      )}
     </div>
   );
 }
